e0e1a03feae6dfe21c46551f6da6dbf8f8f2d3a1
Frigge Steyaert Jamain <frigge.steyaert.jamain@student.howest.be>
2020-10-31 15:49:26 +0100

    created database migrations

---
+++ database/migrations/2020_10_30_120356_create_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->string('title', 255);
+            $table->string('description', 500);
+            $table->enum('type', ['laborer', 'clerk']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
